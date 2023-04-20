20d7623cc4a9812b079c530af93cc9722b71335d
Edward Tanguay (windows) <edward@tanguay.info>
2023-04-20 08:10:27 +0200

    top-level await out

--- main.js
+++ main.js
@@ -10,19 +10,21 @@ const config = {
 const jobsUrl = 'https://edwardtanguay.vercel.app/share/jobs.json';
 const skillsUrl = 'https://edwardtanguay.vercel.app/share/skills.json';
 
-const jobs = (await axios.get(jobsUrl)).data;
-const skills = (await axios.get(skillsUrl)).data;
+(async () => {
+	const jobs = (await axios.get(jobsUrl)).data;
+	const skills = (await axios.get(skillsUrl)).data;
 
-const getJobAdHtml = (skill) => {
-	const jobHtmlBlocks = skill.jobs.map(
-		(job) => `<span><a target="_blank" href="${job.url}">${job.title} at ${job.company}</a></span>`
-	);
-	return jobHtmlBlocks.join('<span> &#x2022; </span>');
-};
+	const getJobAdHtml = (skill) => {
+		const jobHtmlBlocks = skill.jobs.map(
+			(job) =>
+				`<span><a target="_blank" href="${job.url}">${job.title} at ${job.company}</a></span>`
+		);
+		return jobHtmlBlocks.join('<span> &#x2022; </span>');
+	};
 
-const displaySkill = (skill) => {
-	let html = '';
-	html += `
+	const displaySkill = (skill) => {
+		let html = '';
+		html += `
 <div class="skill">
     <div class="name">${skill.name}</div>
 	<div class="extraInfo" style="display:none">
@@ -47,71 +49,76 @@ const displaySkill = (skill) => {
 			}" class="learnLink">videos</a>
 		</div>
 		<div class="jobAds">
-		<span class="intro">${skill.jobs.length === 1 ? '1 job ad' : skill.jobs.length + ' job ads'}: </span>
+		<span class="intro">${
+			skill.jobs.length === 1
+				? '1 job ad'
+				: skill.jobs.length + ' job ads'
+		}: </span>
 			${getJobAdHtml(skill)}	
 		</div>
 	</div>
 </div>
  `;
-	return html;
-};
+		return html;
+	};
 
-const determineCorrectAnswer = (randomSkills) => {
-	const randomIndex = Math.floor(Math.random() * randomSkills.length);
-	config.correctSkill = randomSkills[randomIndex];
-};
+	const determineCorrectAnswer = (randomSkills) => {
+		const randomIndex = Math.floor(Math.random() * randomSkills.length);
+		config.correctSkill = randomSkills[randomIndex];
+	};
 
-const getJobsForSkill = (skill) => {
-	return jobs.filter(job => {
-		const skillIdCodes = job.skillList.split(',');
-		for (const skillIdCode of skillIdCodes) {
-			if (skill.idCode === skillIdCode.trim()) {
-				return job;
-			}
-		}
-	});
-}
-
-const getQuizSkills = () => {
-	const randomSkills = [];
-	for (let i = 0; i < config.numberOfQuestions; i++) {
-		const randomIndex = Math.floor(Math.random() * skills.length);
-		const randomSkill = skills[randomIndex];
-		randomSkill.jobs = getJobsForSkill(randomSkill);
-		randomSkills.push(randomSkill);
-	}
-	determineCorrectAnswer(randomSkills);
-	return randomSkills;
-};
-
-const displayQuizHtml = () => {
-	let html = '<div class="quizArea">';
-	for (const skill of getQuizSkills()) {
-		html += displaySkill(skill);
-	}
-	html += '</div>';
-	return html;
-};
-
-const attachEvents = () => {
-	const btnToggleAnswersElem = document.querySelector('.btnToggleAnswers');
-	btnToggleAnswersElem.addEventListener('click', () => {
-		const extraInfoElems = document.querySelectorAll('.extraInfo');
-		config.displayingExtraInfo = !config.displayingExtraInfo;
-		extraInfoElems.forEach((elem) => {
-			if (config.displayingExtraInfo) {
-				elem.style.display = 'block';
-			} else {
-				elem.style.display = 'none';
+	const getJobsForSkill = (skill) => {
+		return jobs.filter((job) => {
+			const skillIdCodes = job.skillList.split(',');
+			for (const skillIdCode of skillIdCodes) {
+				if (skill.idCode === skillIdCode.trim()) {
+					return job;
+				}
 			}
 		});
-	});
+	};
 
-	const answerElem = document.querySelector('.answer');
-	answerElem.innerText = config.correctSkill.description;
-};
+	const getQuizSkills = () => {
+		const randomSkills = [];
+		for (let i = 0; i < config.numberOfQuestions; i++) {
+			const randomIndex = Math.floor(Math.random() * skills.length);
+			const randomSkill = skills[randomIndex];
+			randomSkill.jobs = getJobsForSkill(randomSkill);
+			randomSkills.push(randomSkill);
+		}
+		determineCorrectAnswer(randomSkills);
+		return randomSkills;
+	};
 
-document.querySelector('#app').innerHTML = `
+	const displayQuizHtml = () => {
+		let html = '<div class="quizArea">';
+		for (const skill of getQuizSkills()) {
+			html += displaySkill(skill);
+		}
+		html += '</div>';
+		return html;
+	};
+
+	const attachEvents = () => {
+		const btnToggleAnswersElem =
+			document.querySelector('.btnToggleAnswers');
+		btnToggleAnswersElem.addEventListener('click', () => {
+			const extraInfoElems = document.querySelectorAll('.extraInfo');
+			config.displayingExtraInfo = !config.displayingExtraInfo;
+			extraInfoElems.forEach((elem) => {
+				if (config.displayingExtraInfo) {
+					elem.style.display = 'block';
+				} else {
+					elem.style.display = 'none';
+				}
+			});
+		});
+
+		const answerElem = document.querySelector('.answer');
+		answerElem.innerText = config.correctSkill.description;
+	};
+
+	document.querySelector('#app').innerHTML = `
   <div>
   <h1>Webdev Skill Quiz</h1>
   <div class="commandArea">
@@ -122,4 +129,5 @@ document.querySelector('#app').innerHTML = `
   </div>
 `;
 
-attachEvents();
+	attachEvents();
+})();

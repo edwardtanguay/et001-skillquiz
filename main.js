438013c1273146e660c2bf92e81d8bc292ea0934
Edward Tanguay (windows) <edward@tanguay.info>
2023-04-20 04:42:34 +0200

    showing skills

--- main.js
+++ main.js
@@ -2,13 +2,28 @@ import './style.scss';
 import axios from 'axios';
 
 const jobsUrl = 'https://edwardtanguay.vercel.app/share/jobs.json';
+const skillsUrl = 'https://edwardtanguay.vercel.app/share/skills.json';
 
 const jobs = (await axios.get(jobsUrl)).data;
-console.log(jobs);
+const skills = (await axios.get(skillsUrl)).data;
+
+const displayQuizHtml = () => {
+	let html = '<div class="quizArea">';
+	for (const skill of skills) {
+		html += `
+<div class="skill">
+    <div class="name">${skill.name}</div>
+    <div class="description">${skill.description}</div>
+</div>
+ `;
+	}
+  html += '</div>';
+	return html;
+};
 
 document.querySelector('#app').innerHTML = `
   <div>
   <h1>Webdev Skill Quiz</h1>
-  <p>Welcome to this site.</p>
+  ${displayQuizHtml()}
   </div>
 `;

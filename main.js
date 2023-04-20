fd72ebf7eff22ed37e184390817b173348fa6c56
Edward Tanguay (windows) <edward@tanguay.info>
2023-04-20 04:00:58 +0200

    step 1

--- main.js
+++ main.js
@@ -1,4 +1,15 @@
 import './style.scss';
+import axios from 'axios';
+
+const jobsUrl = 'https://edwardtanguay.vercel.app/share/jobs.json';
+
+const getData = async () => {
+	const response = await axios.get(jobsUrl);
+	const jobs = response.data;
+	console.log(jobs);
+}
+
+getData();
 
 document.querySelector('#app').innerHTML = `
   <div>
